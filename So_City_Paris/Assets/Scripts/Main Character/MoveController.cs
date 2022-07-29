1d49141e1af8481e59ea2e9133da75e9820ed08e
NikoloPolo <petruchok02@mail.ru>
2022-07-29 09:04:14 +0300

    1 lvl Devlop v0.4
    Change jump logic

--- So_City_Paris/Assets/Scripts/Main Character/MoveController.cs
+++ So_City_Paris/Assets/Scripts/Main Character/MoveController.cs
@@ -1,5 +1,3 @@
-using System;
-using System.Collections;
 using UnityEngine;
 
 public class MoveController : MonoBehaviour
@@ -9,9 +7,9 @@ public class MoveController : MonoBehaviour
     private Rigidbody2D _rb;
     private bool _isJump;
     [SerializeField] private float _walkSpeed = 3f;
-    [SerializeField] private float _jumpDuration;
-    [SerializeField] private AnimationCurve _yAnimation;
-    [SerializeField] private int _jumpHeight;
+    [SerializeField] private float _jumpForce;
+    private float _move;
+    
 
     void Start()
     {
@@ -21,14 +19,14 @@ public class MoveController : MonoBehaviour
     }
     private void Update()
     {
+        _move = Input.GetAxis("Horizontal");
+        _tr.position += new Vector3(_move * Time.deltaTime * _walkSpeed, 0, 0);
         if (Input.GetKeyDown(KeyCode.Space))
             _isJump = true;
     }
     void FixedUpdate()
     {
-        float move = Input.GetAxis("Horizontal") * Time.deltaTime * _walkSpeed;
-        transform.position += new Vector3(move, 0, 0);
-        CheckPlayerDirection(move);
+        CheckPlayerDirection(_move);
         if (_isJump && CheckGround.isGround)
         {
             Jump();
@@ -37,26 +35,32 @@ public class MoveController : MonoBehaviour
     }
     void Jump()
     {
-        StartCoroutine(JumpPositionCalculate());
+        _rb.AddForce(new Vector2(0, _jumpForce));
     }
-    private IEnumerator JumpPositionCalculate() // My super jump which not working :(
-    {
-        float expiredSeconds = 0f;
-        float progress = 0f;
-        Vector3 startPosition = _tr.position;
-        do
-        {
-            expiredSeconds += Time.deltaTime;
-            progress = expiredSeconds / _jumpDuration;
-            _tr.position = new Vector3(_tr.position.x, startPosition.y + _yAnimation.Evaluate(progress) * _jumpHeight);
-            yield return new WaitForFixedUpdate(); 
-        } while (!CheckGround.isGround && progress<0.42f);
-    }
+    #region
+    //private IEnumerator JumpPositionCalculate() // My super jump which not working :(
+    //{
+    //    float expiredSeconds = 0f;
+    //    float progress = 0f;
+    //    Vector3 startPosition = _tr.position;
+    //    do
+    //    {
+    //        expiredSeconds += Time.deltaTime;
+    //        progress = expiredSeconds / _jumpDuration;
+    //        _tr.position = new Vector3(_tr.position.x, startPosition.y + JumpCalculateY(expiredSeconds) * _jumpHeight);
+    //        yield return null; 
+    //    } while (!CheckGround.isGround);
+    //}
+    //float JumpCalculateY(float x)
+    //{
+    //    return (-1 * Mathf.Pow((x - 2), 2) + 4);
+    //}
+    #endregion
     void CheckPlayerDirection(float move)
     {
         if (!Mathf.Approximately(move, 0))
         {
-            _animator.SetFloat("Speed", Mathf.Abs(Mathf.Round(move * 1000)));
+            _animator.SetFloat("Speed", Mathf.Abs(Mathf.Round(move)));
             Flip(move);
         }
     }
