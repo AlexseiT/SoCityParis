e6ae519ba920cf993e377926a622d97ffc0c569f
Ip17 <78462900+Nikolai-ip@users.noreply.github.com>
2022-09-13 20:03:34 +0300

    Add weapon rotate logic

--- So_City_Paris/Assets/Scripts/Architecture/Weapon/WeaponRotateController.cs
+++ So_City_Paris/Assets/Scripts/Architecture/Weapon/WeaponRotateController.cs
@@ -1,20 +1,28 @@
 using UnityEngine;
+using UnityEngine.UIElements;
 
 internal class WeaponRotateController : MonoBehaviour
 {
-    [SerializeField] protected float rotateGun;
-
+    [SerializeField] private Transform _shouldTr;
     private bool _checkFlip = true;
 
     private void Update()
     {
+
+        Vector3 dir = Camera.main.ScreenToWorldPoint(Input.mousePosition) - _shouldTr.position;
+        float length = (transform.position - _shouldTr.position).magnitude;
+        Vector3 newHandCoordinate = new Vector3(GetAxis(length, dir.x,dir.y), GetAxis(length, dir.y,dir.x), 0)+_shouldTr.position;
+        transform.position = newHandCoordinate;
         Vector3 vectorPoint = Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position;
-        float rotateZ = Mathf.Atan2(vectorPoint.y, vectorPoint.x) * Mathf.Rad2Deg;
-        transform.rotation = Quaternion.Euler(0f, 0f, rotateZ + rotateGun);
+        float rotateZ = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.Euler(0f, 0f, rotateZ);
         Rotate();
     }
     
-
+    private float GetAxis(float length, float val1, float val2)
+    {
+        return (float)length * val1 / (Mathf.Sqrt(val1 * val1 + val2 * val2));
+    }
     public void Rotate()
     {
         Vector3 vectorPoint = Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position;
